595d61ab67e13bbcf78b770782bad84d62c3e041
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-08-07 01:05:55 -0300

    feat: added the loggedIn check

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,15 +2,24 @@ import * as S from "./style";
 import Icon from "../../assets/imgs/icons/Icon.webp";
 import { DateTime } from "luxon";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { Dispatch, SetStateAction } from "react";
 
 interface HeaderProps {
-  path: "home" | "settings" | "mangas" | "favorites" | "users";
+  path: "home" | "settings" | "favorites" | "New Manga";
+  setLogged: Dispatch<SetStateAction<boolean>>;
 }
 
 const actualDate = DateTime.now().setLocale("en");
 const formatedDate = `${actualDate.weekdayShort}, ${actualDate.day} ${actualDate.monthLong} ${actualDate.year}`;
 
-const Header = ({ path }: HeaderProps) => {
+const Header = ({ path, setLogged }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setLogged(false);
+    navigate("/login");
+  };
   return (
     <header>
       <S.HeaderContainer>
@@ -33,11 +42,11 @@ const Header = ({ path }: HeaderProps) => {
           <S.Box href="/">
             <S.Link>Home</S.Link>
           </S.Box>
-          <S.Box href="/mangas">
-            <S.Link>New Manga</S.Link>
-          </S.Box>
           <S.Box href="/favorites">
             <S.Link>Favorites</S.Link>
+          </S.Box>
+          <S.Box href="/mangas">
+            <S.Link>New Manga</S.Link>
           </S.Box>
           <S.Box href="/settings">
             <S.Link>Settings</S.Link>
