0852a5f385436c21907a518c224e4fa75aaab24b
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-08-07 23:14:09 -0300

    feat(login): added login page

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -3,8 +3,8 @@ import { Theme } from "../../types/styled-components";
 
 interface StyledButtonProps {
   theme: Theme;
-  variant?: "disabled" | "cancel" | false;
-  size?: "small" | "large" | false;
+  variant?: "disabled" | "cancel";
+  size?: "small" | "large" | "x-large";
 }
 
 export const StyledButton = styled.button<StyledButtonProps>`
@@ -18,6 +18,7 @@ export const StyledButton = styled.button<StyledButtonProps>`
     cursor: pointer;
     font-family: ${theme.constants.bodyFontFamily};
     font-weight: 500;
+
     ${variant === "disabled" &&
     css`
       background-color: ${theme.colors.baseBg2};
@@ -25,6 +26,7 @@ export const StyledButton = styled.button<StyledButtonProps>`
       border: 1px solid ${theme.colors.baseBg1};
       box-shadow: none;
     `}
+
     ${variant === "cancel" &&
     css`
       background-color: ${theme.colors.baseBg2};
@@ -32,13 +34,20 @@ export const StyledButton = styled.button<StyledButtonProps>`
       border: 1px solid ${theme.colors.secondaryColor};
       box-shadow: none;
     `}
+
     ${size === "small" &&
     css`
       height: 2.125rem;
       padding: 0 0.75rem;
       width: 7.25rem;
     `}
+
     ${size === "large" &&
+    css`
+      width: 18.625rem;
+    `}
+
+    ${size === "x-large" &&
     css`
       width: 22.5rem;
     `}
